22337a8d4b57484ba0a0bfaa079218cc3fe12cc3
Enis <enis.mema@gmail.com>
2023-05-25 13:11:13 +0200

    Add method for 0 to be 1

--- solver.rb
+++ solver.rb
@@ -1,6 +1,8 @@
 class Solver
   def factorial(number)
     raise ArgumentError if number.negative?
+    return 1 if number.zero?
+
   end
     
 
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -10,7 +10,7 @@ describe Solver do
   end
 
   it 'Test when number is 0 to be 1' do
-    expect (@solver.factorial(0)).to eq(1)
+    expect(@solver.factorial(0)).to eq(1)
   end
 
 end
