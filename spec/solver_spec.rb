3b765eaed965a9ac7205305aa9abfa0436bdaee6
mumo-esther <mumoesther2021@gmail.com>
2023-05-25 16:05:36 +0300

    Add Fizzbuzz method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -22,26 +22,24 @@ describe Solver do
   end
 
 it 'returns "fizz" when N is divisible by 3' do
-    expect(solver.fizzbuzz(3)).to eq('fizz')
-    expect(solver.fizzbuzz(9)).to eq('fizz')
-    expect(solver.fizzbuzz(12)).to eq('fizz')
+    
+    expect(@solver.fizzbuzz(9)).to eq('fizz')
+    
   end
 
   it 'returns "buzz" when N is divisible by 5' do
-    expect(solver.fizzbuzz(5)).to eq('buzz')
-    expect(solver.fizzbuzz(10)).to eq('buzz')
-    expect(solver.fizzbuzz(20)).to eq('buzz')
+    expect(@solver.fizzbuzz(5)).to eq('buzz')
+    
   end
 
   it 'returns "fizzbuzz" when N is divisible by both 3 and 5' do
-    expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
-    expect(solver.fizzbuzz(30)).to eq('fizzbuzz')
-    expect(solver.fizzbuzz(45)).to eq('fizzbuzz')
+    expect(@solver.fizzbuzz(15)).to eq('fizzbuzz')
+  
   end
 
   it 'returns N as a string for any other case' do
-    expect(solver.fizzbuzz(1)).to eq('1')
-    expect(solver.fizzbuzz(7)).to eq('7')
-    expect(solver.fizzbuzz(19)).to eq('19')
+    
+    expect(@solver.fizzbuzz(7)).to eq('7')
+    
   end
 end
